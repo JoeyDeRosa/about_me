c0a67208f14078f5a4fff72a8562f3fef8a16083
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-09-21 15:22:42 -0700

    fix javascript add html content some css

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ while(playing === 1) {
   var wrong = 0;
   var q = 1;
   while(q === 1) {
-    var question = prompt('First question, was I, Joey DeRosa, born in Washington? y/n').toLowerCase();
-    if(question === 'y' || question === 'yes') {
+    var answer = prompt('First question, was I, Joey DeRosa, born in Washington? y/n').toLowerCase();
+    if(answer === 'y' || answer === 'yes') {
       alert('That is correct, congratulations! I was born in Tacoma Washington to be exact.');
       right++;
       totalRight++;
       q = 0;
       console.log('user got the first question right.');
-    } else if (question === 'n' || question === 'no') {
+    } else if (answer === 'n' || answer === 'no') {
       alert('Ohhhhhh, that is unfortunatly wrong. I was born in Tacoma Washington.');
       wrong++;
       totalWrong++;
@@ -50,14 +50,14 @@ while(playing === 1) {
 
   q = 1;
   while(q === 1) {
-    question = prompt('Second qeustion, is pizza my favorite food? y/n').toLowerCase();
-    if(question === 'n' || question === 'no') {
+    answer = prompt('Second qeustion, is pizza my favorite food? y/n').toLowerCase();
+    if(answer === 'n' || answer === 'no') {
       alert('Wow, you saw through my trick question. Very impressive.');
       right++;
       totalRight++;
       q = 0;
       console.log('user got the second question right.');
-    } else if (question === 'y' || question === 'yes') {
+    } else if (answer === 'y' || answer === 'yes') {
       alert('HAHA, trick question. All food is delicious and how dare you expect me to pick a favorite.');
       wrong++;
       totalWrong++;
@@ -71,14 +71,14 @@ while(playing === 1) {
 
   q = 1;
   while(q === 1) {
-    question = prompt('Third question, is the car I drive a yellow 2002 ford escape. y/n').toLowerCase();
-    if(question === 'y' || question === 'yes') {
+    answer = prompt('Third question, is the car I drive a yellow 2002 ford escape. y/n').toLowerCase();
+    if(answer === 'y' || answer === 'yes') {
       alert('Impressive, I do drive a yellow 2002 ford escape. Not sure how you could possibly know that considering I take the bus here.');
       right++;
       totalRight++;
       q = 0;
       console.log('user got the third question right.');
-    } else if (question === 'n' || question === 'no') {
+    } else if (answer === 'n' || answer === 'no') {
       alert('I\'m sorry but that is incorect. I was hoping the detail of the question would give away the answer but i guess not.');
       wrong++;
       totalWrong++;
@@ -92,14 +92,14 @@ while(playing === 1) {
 
   q = 1;
   while(q === 1) {
-    question = prompt('Fourth question, is the computer i use for code fellows a Mac. y/n').toLowerCase();
-    if(question === 'n' || question === 'no') {
+    answer = prompt('Fourth question, is the computer i use for code fellows a Mac. y/n').toLowerCase();
+    if(answer === 'n' || answer === 'no') {
       alert('Very observent of you, while my laptop is silver like a Mac it is in fact a Google Chromebook running Ubuntu');
       right++;
       totalRight++;
       q = 0;
       console.log('user got the fourth question right.');
-    } else if (question === 'y' || question === 'yes') {
+    } else if (answer === 'y' || answer === 'yes') {
       alert('That answer is not correct. While my laptop is silver like a Mac it is in fact a Google Chromebook running Ubuntu.');
       wrong++;
       totalWrong++;
@@ -113,14 +113,14 @@ while(playing === 1) {
 
   q = 1;
   while(q === 1) {
-    question = prompt('Fifth question, and this is a tough one, is my hair red. y/n').toLowerCase();
-    if(question === 'y' || question === 'yes') {
+    answer = prompt('Fifth question, and this is a tough one, is my hair red. y/n').toLowerCase();
+    if(answer === 'y' || answer === 'yes') {
       alert('Impossible! how could you possibly have known that, I didn\'t think anyone would know that.');
       right++;
       totalRight++;
       q = 0;
       console.log('user got the fifth question right.');
-    } else if (question === 'n' || question === 'no') {
+    } else if (answer === 'n' || answer === 'no') {
       alert('You got the last question wrong, but don\'t worry I didn\'t expect anyone to get this question right.');
       wrong++;
       totalWrong++;
@@ -133,14 +133,14 @@ while(playing === 1) {
   }
 
   for(var i = 0; i < 4; i++) {
-    question = parseFloat(prompt('Sixth question, can you guess my favorite number, you have four tries.'));
-    if(question === 11) {
+    answer = parseFloat(prompt('Sixth question, can you guess my favorite number, you have four tries.'));
+    if(answer === 11) {
       alert('Nicely done, my favorite number is 11.');
       right++;
       totalRight++;
       i = 4;
       console.log('user got the sixth question right.');
-    } else if(i < 3 && question < 11) {
+    } else if(i < 3 && answer < 11) {
       alert('I\'m afraid that number is to low, you have ' + (3 - i) + ' trie(s) left.');
     } else if(i < 3){
       alert('I\'m afraid that number is to high, you have ' + (3 - i) + ' trie(s) left.');
@@ -152,33 +152,30 @@ while(playing === 1) {
     }
   }
 
-  // var bands = ['the white stripes', 'the rolling stones', 'tigers jaw', 'nirvana', 'slipknot'];
-  // for(var i = 0; i < 6, i++) {
-  //   var question = prompt('Seventh question, can you guess one of my favorite bands. You have six tries.').toLowerCase();
-  //   var a = 0;
-  //   for(var x = 0 ; x < 5; x + 0) {
-  //     if(question === bands[x]) {
-  //       a = band[x];
-  //       x = 5;
-  //     } else {
-  //       x++;
-  //     }
-  //   }
-  //   if(question === bands[a]) {
-  //     alert('Nicely done, ' + bands[a] + ' are/is one of my favorite bands');
-  //     right++;
-  //     totalRight++;
-  //     i = 6;
-  //     console.log('user got the seventh question right.');
-  //   } else if(i < 5) {
-  //       aler('Nope, that is not one of my top five favorite bands. You have ' + (5 - i) + ' tries left');
-  //   } else {
-  //     alert('I\'m afraid you never guessed one of my favorite bands. They are ' + bands);
-  //     wrong++;
-  //     totalWrong++;
-  //     console.log('user got the seventh question wrong.');
-  //   }
-  // }
+  var bands = ['the white stripes', 'the rolling stones', 'tigers jaw', 'nirvana', 'slipknot'];
+  for(i = 0; i < 6; i++) {
+    answer = prompt('Seventh question, can you guess one of my favorite bands. You have six tries.').toLowerCase();
+    var a = 0;
+    for(var x = 0 ; x < 5; x++) {
+      if(answer === bands[x]) {
+        a = bands[x];
+      }
+    }
+    if(answer === a) {
+      alert('Nicely done, ' + a + ' are/is one of my favorite bands');
+      right++;
+      totalRight++;
+      i = 6;
+      console.log('user got the seventh question right.');
+    } else if(i < 5) {
+      alert('Nope, that is not one of my top five favorite bands. You have ' + (5 - i) + ' trie(s) left');
+    } else {
+      alert('I\'m afraid you never guessed one of my favorite bands. They are ' + bands);
+      wrong++;
+      totalWrong++;
+      console.log('user got the seventh question wrong.');
+    }
+  }
 
   alert('Wow, that was fun!');
   alert('this game you got ' + right + ' right and ' + wrong + ' wrong.');
@@ -186,12 +183,12 @@ while(playing === 1) {
 
   q = 1;
   while(q === 1) {
-    question = prompt('Would you like to play again? y/n');
-    if(question === 'y' || question === 'yes') {
+    answer = prompt('Would you like to play again? y/n');
+    if(answer === 'y' || answer === 'yes') {
       alert('Awsome lets get started!');
       q = 0;
       console.log('user wants to play again.');
-    } else if (question === 'n' || question === 'no') {
+    } else if (answer === 'n' || answer === 'no') {
       alert('That\'s to bad.');
       q = 0;
       playing = 0;
