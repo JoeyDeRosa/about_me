5758b263e2755bd5dda933c293ed0bdadf76547d
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-09-22 12:20:02 -0700

    a new file for the repo, just some ideas

--- test.js
+++ test.js
@@ -1,120 +1,63 @@
-// function question(qPrompt, qAlert, qAlert2, answer1, answer2, wAnswer1, wAnswer2) {
-//   this.qPrompt = qPrompt;
-//   this.qAlert1 = qAlert1;
-//   this.qAlert2 = qAlert2;
-//   this.answer1 = answer1;
-//   this.answer2 = answer2;
-//   this.wAnswer1 = wAnswer1;
-//   this.wAnswer2 = wAnswer2;
-// }
+'use strict';
 var qArray = [];
-
-var question1 = {
-    qPrompt: 'First question, was I, Joey DeRosa, born in Washington? y/n',
-    qAlert1: 'That is correct, congratulations! I was born in Tacoma Washington to be exact.',
-    qAlert2: 'Ohhhhhh, that is unfortunatly wrong. I was born in Tacoma Washington.',
-    answer1: 'y',
-    answer2: 'yes',
-    wAnswer1: 'n',
-    wAnswer2: 'no',
+function Question(qPrompt, alert1, alert2, answer, wAnswer) {
+  this.qPrompt = qPrompt;
+  this.alert1 = alert1;
+  this.alert2 = alert2;
+  this.answer = answer;
+  this.wAnswer = wAnswer;
+  qArray.push(this);
 }
-qArray.push(question1);
-
-var question2 = {
-    qPrompt: '',
-    qAlert1: '',
-    qAlert2: '',
-    answer1: 'n',
-    answer2: 'no',
-    wAnswer1: 'y',
-    wAnswer2: 'yes',
-}
-qArray.push(question2);
-
-var question3 = {
-    qPrompt: '',
-    qAlert1: '',
-    qAlert2: '',
-    answer1: 'y',
-    answer2: 'yes',
-    wAnswer1: 'n',
-    wAnswer2: 'no',
-}
-qArray.push(question3);
-
-var question4 = {
-    qPrompt: '',
-    qAlert1: '',
-    qAlert2: '',
-    answer1: '',
-    answer2: '',
-    wAnswer1: '',
-    wAnswer2: '',
-}
-qArray.push(question4);
-
-var question5 = {
-    qPrompt: '',
-    qAlert1: '',
-    qAlert2: '',
-    answer1: '',
-    answer2: '',
-    wAnswer1: '',
-    wAnswer2: '',
-}
-qArray.push(question5);
-
 var qIndex = 0;
 var totalRight = 0;
 var totalWrong = 0;
 var games = 0;
 
-while(playing === 1) {
+var user = prompt('What is your name');
+
+do {
+  
+}
   var right = 0;
   var wrong = 0;
   games++;
 
-  while(qIndex < qArray.length) {
+  function askQuestion(qArray) {
     var a = prompt(qArray[qIndex].qPrompt).toLowerCase();
-    if(a === qArray[qIndex].answer1 || a === qArray[qIndex].answer2) {
-      alert(qArray[qIndex].qAlert1);
-      console.log('user got the ' + (qIndex + 1) + 'question wrong.');
-      qIndex++;
-      right++;
-      totalRight++;
-    } else if(a === qArray[qIndex].wAnswer1 || a === qArray[qIndex].wAnswer2) {
-      alert(qArray[qIndex].qAlert2);
-      console.log('user got the ' + (qIndex + 1) + 'question wrong.');
-      qIndex++;
-      wrong++;
-      totalWrong++;
+    if(a === qArray[qIndex].answer || a === qArray[qIndex].answer.charAt(0)) {
+      alert(qArray[qIndex].alert1);
+      console.log('user got the ' + (qIndex + 1) + 'question right.');
+      return true;
+    } else if(a === qArray[qIndex].wAnswer || a === qArray[qIndex].wAnswer.charAt(0)) {
+      alert(qArray[qIndex].alert2);
+      console.log(user + ' got the ' + (qIndex + 1) + 'question wrong.');
+      return false;
     } else {
       alert('I\'m afraid that is not an answer, try again.');
       console.log('user entered an invalid responce.');
     }
   }
 
-  alert('Wow, that was fun!');
-  alert('this game you got ' + right + ' right and ' + wrong + ' wrong.');
-  alert('In ' + games + ' game(s) you have '+ totalRight + ' right and ' + totalWrong + ' wrong.');
+alert('Wow, that was fun!');
+alert('this game you got ' + right + ' right and ' + wrong + ' wrong.');
+alert('In ' + games + ' game(s) you have ' + totalRight + ' right and ' + totalWrong + ' wrong.');
 
-  var x = 1;
-  while(x == 1) {
-    var a = prompt('Would you like to play again?');
-    if(question === 'y' || question === 'yes') {
-      alert('Awsome lets get started!');
-      console.log('user wants to play again.');
-      x++;
-      qIndex = 0;
-    } else if (question === 'n' || question === 'no') {
-      alert('That\'s to bad.');
-      x++;
-      playing = 0;
-      console.log('user does not want to play again.');
-    } else {
-      alert('I\'m afraid that is not an answer, try again.');
-      console.log('user entered an invalid responce.');
-    }
+var x = 1;
+while(x == 1) {
+  a = prompt('Would you like to play again?');
+  if(question === 'y' || question === 'yes') {
+    alert('Awsome lets get started!');
+    console.log('user wants to play again.');
+    x++;
+    qIndex = 0;
+  } else if (question === 'n' || question === 'no') {
+    alert('That\'s to bad.');
+    x++;
+    playing = 0;
+    console.log('user does not want to play again.');
+  } else {
+    alert('I\'m afraid that is not an answer, try again.');
+    console.log('user entered an invalid responce.');
   }
 }
 
